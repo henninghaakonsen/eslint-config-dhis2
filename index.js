6ce3922578bbaa8dbeb90bd616191d41d4088602
Jen Jones Arnesen <jjarnesen@gmail.com>
2017-09-12 15:04:01 +0200

    Ignore the AirBnb eslintrc (#2)
    * Do not use AirBnb eslintrc file
    
    * increment patch version

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const path = require('path');
 const stripComments = require('strip-json-comments');
 const fs = require('fs');
 
-// add the rules from the eslintrc files
-const rules = ['node_modules/eslint-config-airbnb/.eslintrc', '.eslintrc'].reduce((allRules, file) => {
-    const filename = path.join(__dirname, file);
-    const data = stripComments(fs.readFileSync(filename, { encoding: 'utf-8' }));
-    return Object.assign(allRules, JSON.parse(data).rules);
-}, {});
+const filename = path.join(__dirname, '.eslintrc');
+const data = stripComments(fs.readFileSync(filename, { encoding: 'utf-8' }));
+const rules = JSON.parse(data).rules;
 
 module.exports = Object.assign(AirBnBConfig, { rules });
